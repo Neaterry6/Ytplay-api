702e0fa01a2345a781995200b2a40fc0a9eb34e5
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-09-21 09:51:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import yt_dlp
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def play():
         return jsonify({'status': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
